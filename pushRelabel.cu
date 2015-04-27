c878ef8d7380e294e0182dd577ca7e9aa44e9624
miyazaki toshimasa <rizen.1.27@gmail.com>
2015-04-27 16:01:45 +0900

    途中

--- pushRelabel.cu
+++ pushRelabel.cu
@@ -13,9 +13,33 @@ struct node{
 };
 struct node *top;
 struct node *btm;
-
+void enqueue(unsigned int id){
+    struct node *node = new struct node;
+    node->id = id;
+    node->prev = NULL;
+    if (top == NULL) btm = node;
+    else top-> prev = node;
+    node->next = top;
+    top = node;
+}
+unsgined int dequeue(void){
+        struct node *node = btm;
+        unsgined int id = node->id;
+        btm = node->prev;
+        if(btm ==NULL) top = NULL;
+        else btm->next = NULL;
+        delete node;
+        return id;
+}
 int main(int argc, char** argv){
+    FILE *fp;
+    char *fname
+    if(){
+    }else{
+        fname = "sample.inp";
+    }
     printf("the cake is a lie");
+    fclose(fp);
     return 0;
 }
 void fopenDIMACS(FILE *fp,int fSize){
