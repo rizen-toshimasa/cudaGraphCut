7e41260493bb2b184f52c811a811892b9cdbc8de
miyazaki toshimasa <rizen.1.27@gmail.com>
2015-05-11 16:37:00 +0900

    先生に言われて,データ構造前書き換えし始めた

--- pushRelabel.cu
+++ pushRelabel.cu
@@ -1,36 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-struct data{
-    unsigned int o;//overflow value
-    unsigned int h;//height value
-    unsigned char f;//first flg
-};
-struct node{
-    struct node *next;
-    struct node *prev;
-    struct data *data;
-};
-struct node *top;
-struct node *btm;
-void enqueue(struct data *data){
-    struct node *node = new struct node;
-    node->data = data;
-    node->prev = NULL;
-    if (top == NULL) btm = node;
-    else top-> prev = node;
-    node->next = top;
-    top = node;
+typedef struct Node{
+    struct Edge *in_edge;
+    struct Edge *out_edge;
+    unsigned int flow;
+    unsigned int height;
+} node_t;
+typedef struct Edge{
+    struct Edge *in_next;
+    struct Edge *out_next;
+    struct Node *push;
+    struct Node *pull;
+    struct Edge *rev;
+    struct int flow;
+} edge_t;
+void Push(struct Node *node_push, struct Node *node_pull, int flow){
+    struct Edge edge = new edge;
+    struct Edge temp_edge = node_push->out_edge;
+    edge.in_next = 1;
 }
-struct data  dequeue(void){
-        struct node *node = btm;
-        struct data *data = node->data;
-        btm = node->prev;
-        if(btm ==NULL) top = NULL;
-        else btm->next = NULL;
-        delete node;
-        return *data;
+bool IsExistEdge(struct Node node_push, struct Node node_pull){
+    
 }
+
 int main(int argc, char** argv){
     FILE *fp;
     const char fname[] = "sample.inp";
@@ -38,10 +31,22 @@ int main(int argc, char** argv){
         printf("ファイル読み込みエラー\n");
         exit(EXIT_FAILURE);
     }
-    char str[256];
-    while(fgets(str, 256, fp) != NULL){
-        printf("%s",str);
+    int size_nodes, num_edge;
+    fscanf(fp, "p max %d %d", &size_nodes, &num_edge);
+    printf("Node:%d Edge:%d\n", size_nodes, num_edge);
+    int num_src;
+    int num_sink;
+    fscanf(fp, "n %d s", &num_src);
+    fscanf(fp, "n %d t", &num_sink);
+    struct Node *nodes;
+    nodes = (struct Node *)malloc(sizeof (struct Node) * node_max_num);
+    int push, pull, flow;
+    while(fscanf(fp, "a %d %d %d",&node_push, &node_pull, &flow) == NULL){
+        //NodeHoge(nodes, num_max_node, push, pull, flow);
+        if(!IsExistEdge(node_push, node_pull)) continue;
+        Push(node_push, node_pull, flow);
     }
+    free(edgeArray)
     fclose(fp);
     return 0;
 }
